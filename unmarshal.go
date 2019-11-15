7aa2f6063039f911b51542dc18cce059486b1241
Peter Wood <peter@alastria.net>
2019-11-15 21:05:24 +0000

    Add support for uint unmarshalling.

--- unmarshal.go
+++ unmarshal.go
@@ -11,19 +11,33 @@ func Unmarshall(data []byte, v interface{}) (err error) {
 
 	val := reflect.Indirect(reflect.ValueOf(v))
 
-	if err = unmarshalValue(bb, v, "root", val, ""); err != nil {
+	if !val.CanSet() {
+		return fmt.Errorf("cannot unmarshall to non pointer")
+	}
+
+	if err = unmarshalValue(bb, "root", val, ""); err != nil {
 		return
 	}
 
 	return
 }
 
-func unmarshalValue(bb *bytes.Buffer, v interface{}, name string, value reflect.Value, tags reflect.StructTag) (err error) {
+func unmarshalValue(bb *bytes.Buffer, name string, value reflect.Value, tags reflect.StructTag) (err error) {
 	kind := value.Kind()
 
+	endian := tagEndianness(tags)
+
 	switch kind {
+	case reflect.Uint8:
+		err = unmarshallUint(bb, endian, 1, value)
+	case reflect.Uint16:
+		err = unmarshallUint(bb, endian, 2, value)
+	case reflect.Uint32:
+		err = unmarshallUint(bb, endian, 4, value)
+	case reflect.Uint64:
+		err = unmarshallUint(bb, endian, 8, value)
 	case reflect.Struct:
-		err = unmarshalStruct(bb, v, value)
+		err = unmarshalStruct(bb, value)
 	default:
 		err = fmt.Errorf("%w: field '%s' of type '%v'", UnsupportedType, name, kind)
 	}
@@ -31,7 +45,7 @@ func unmarshalValue(bb *bytes.Buffer, v interface{}, name string, value reflect.
 	return
 }
 
-func unmarshalStruct(bb *bytes.Buffer, v interface{}, value reflect.Value) error {
+func unmarshalStruct(bb *bytes.Buffer, value reflect.Value) error {
 	structType := value.Type()
 
 	for i := 0; i < value.NumField(); i++ {
@@ -40,10 +54,40 @@ func unmarshalStruct(bb *bytes.Buffer, v interface{}, value reflect.Value) error
 		tags := field.Tag
 		name := field.Name
 
-		if err := unmarshalValue(bb, v, name, value, tags); err != nil {
+		if err := unmarshalValue(bb, name, value, tags); err != nil {
 			return err
 		}
 	}
 
+	return nil
+}
+
+func unmarshallUint(bb *bytes.Buffer, endian EndianTag, size uint8, value reflect.Value) error {
+	readValue := uint64(0)
+
+	switch endian {
+	case BigEndian:
+		for i := uint8(0); i < size; i++ {
+			readByte, err := bb.ReadByte()
+			if err != nil {
+				return err
+			}
+
+			shiftOffset := (size - i - 1) * 8
+			readValue |= uint64(readByte) << shiftOffset
+		}
+	case LittleEndian:
+		for i := uint8(0); i < size; i++ {
+			readByte, err := bb.ReadByte()
+			if err != nil {
+				return err
+			}
+
+			shiftOffset := i * 8
+			readValue |= uint64(readByte) << shiftOffset
+		}
+	}
+
+	value.SetUint(readValue)
 	return nil
 }
--- unmarshal_test.go
+++ unmarshal_test.go
@@ -13,12 +13,23 @@ func TestUnmarshall(t *testing.T) {
 		}
 
 		var data []byte
-		instance := StructUnderTest{}
+		instance := &StructUnderTest{}
 		err := Unmarshall(data, instance)
 
 		assert.Error(t, err)
 		assert.True(t, errors.Is(err, UnsupportedType))
 		assert.Equal(t, "unsupported type: field 'One' of type 'chan'", err.Error())
+	})
+
+	t.Run("verify non pointers raise an error", func(t *testing.T) {
+		type StructUnderTest struct {
+			One uint8
+		}
+
+		actualStruct := StructUnderTest{}
+		err := Unmarshall([]byte{0x00}, actualStruct)
+
+		assert.Error(t, err)
 	})
 
 	t.Run("verify byte and uint8 unmarshals", func(t *testing.T) {
@@ -31,7 +42,97 @@ func TestUnmarshall(t *testing.T) {
 		data, _ := Marshall(expectedStruct)
 
 		actualStruct := StructUnderTest{}
-		err := Unmarshall(data, actualStruct)
+		err := Unmarshall(data, &actualStruct)
+
+		assert.NoError(t, err)
+		assert.Equal(t, expectedStruct, actualStruct)
+	})
+
+	t.Run("verify uint16 LE is unmarshalled", func(t *testing.T) {
+		type StructUnderTest struct {
+			One uint16 `bcendian:"little"`
+		}
+
+		expectedStruct := StructUnderTest{One: 0x8001}
+		data, _ := Marshall(expectedStruct)
+
+		actualStruct := StructUnderTest{}
+		err := Unmarshall(data, &actualStruct)
+
+		assert.NoError(t, err)
+		assert.Equal(t, expectedStruct, actualStruct)
+	})
+
+	t.Run("verify uint16 BE is unmarshalled", func(t *testing.T) {
+		type StructUnderTest struct {
+			One uint16 `bcendian:"big"`
+		}
+
+		expectedStruct := StructUnderTest{One: 0x8001}
+		data, _ := Marshall(expectedStruct)
+
+		actualStruct := StructUnderTest{}
+		err := Unmarshall(data, &actualStruct)
+
+		assert.NoError(t, err)
+		assert.Equal(t, expectedStruct, actualStruct)
+	})
+
+	t.Run("verify uint32 LE is unmarshalled", func(t *testing.T) {
+		type StructUnderTest struct {
+			One uint32 `bcendian:"little"`
+		}
+
+		expectedStruct := StructUnderTest{One: 0x80010203}
+		data, _ := Marshall(expectedStruct)
+
+		actualStruct := StructUnderTest{}
+		err := Unmarshall(data, &actualStruct)
+
+		assert.NoError(t, err)
+		assert.Equal(t, expectedStruct, actualStruct)
+	})
+
+	t.Run("verify uint32 BE is unmarshalled", func(t *testing.T) {
+		type StructUnderTest struct {
+			One uint32 `bcendian:"big"`
+		}
+
+		expectedStruct := StructUnderTest{One: 0x80010203}
+		data, _ := Marshall(expectedStruct)
+
+		actualStruct := StructUnderTest{}
+		err := Unmarshall(data, &actualStruct)
+
+		assert.NoError(t, err)
+		assert.Equal(t, expectedStruct, actualStruct)
+	})
+
+	t.Run("verify uint64 LE is unmarshalled", func(t *testing.T) {
+		type StructUnderTest struct {
+			One uint64 `bcendian:"little"`
+		}
+
+		expectedStruct := StructUnderTest{One: 0x8001020304050607}
+		data, _ := Marshall(expectedStruct)
+
+		actualStruct := StructUnderTest{}
+		err := Unmarshall(data, &actualStruct)
+
+		assert.NoError(t, err)
+		assert.Equal(t, expectedStruct, actualStruct)
+	})
+
+	t.Run("verify uint64 BE is unmarshalled", func(t *testing.T) {
+		type StructUnderTest struct {
+			One uint64 `bcendian:"big"`
+		}
+
+		expectedStruct := StructUnderTest{One: 0x8001020304050607}
+		data, _ := Marshall(expectedStruct)
+
+		actualStruct := StructUnderTest{}
+		err := Unmarshall(data, &actualStruct)
 
 		assert.NoError(t, err)
 		assert.Equal(t, expectedStruct, actualStruct)
