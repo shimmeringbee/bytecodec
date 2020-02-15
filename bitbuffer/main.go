517fffb5e57e2f9c577e06b0bebc2d20a6c28665
Peter Wood <peter@alastria.net>
2020-02-15 22:26:44 +0000

    Add reading bit functionality to bit buffer, ensure the tests run on github.

--- bitbuffer/main.go
+++ bitbuffer/main.go
@@ -43,12 +43,49 @@ func (bb *BitBuffer) WriteByte(c byte) error {
 }
 
 func (bb *BitBuffer) ReadBits(bitCount int) (byte, error) {
-	return 0, nil
+	if bitCount > maxBitOperations {
+		return 0, ErrorTooManyBitsInOperation
+	}
+
+	if bb.offset == 0 && bitCount == 8 {
+		return bb.buf.ReadByte()
+	}
+
+	retVal := byte(0)
+
+	for i := 0; i < bitCount; i++ {
+		if bb.offset == 0 {
+			unhandled, err := bb.buf.ReadByte()
+
+			if err != nil {
+				return 0, err
+			}
+
+			bb.unhandled = unhandled
+			bb.offset = 8
+		}
+
+		bit := bb.unhandled&0x80 == 0x80
+		bb.unhandled = bb.unhandled << 1
+		bb.offset--
+
+		retVal = retVal << 1
+
+		if bit {
+			retVal = retVal | 1
+		}
+	}
+
+	return retVal, nil
 }
 
 func (bb *BitBuffer) WriteBits(bits byte, bitCount int) error {
 	if bitCount > maxBitOperations {
 		return ErrorTooManyBitsInOperation
+	}
+
+	if bb.offset == 0 && bitCount == 8 {
+		return bb.buf.WriteByte(bits)
 	}
 
 	mask := byte(1 << (bitCount - 1))
--- bitbuffer/main_test.go
+++ bitbuffer/main_test.go
@@ -37,4 +37,32 @@ func Test_BitBuffer(t *testing.T) {
 
 		assert.Equal(t, expectedBytes, actualBytes)
 	})
+
+	t.Run("attempting to read more than 8 bits errors", func(t *testing.T) {
+		bb := NewBitBuffer()
+
+		_, err := bb.ReadBits(9)
+		assert.Error(t, err)
+	})
+
+	t.Run("reading bytes works normally", func(t *testing.T) {
+		bb := NewBitBufferFromBytes([]byte{0xaa, 0xdd})
+
+		one, _ := bb.ReadByte()
+		two, _ := bb.ReadByte()
+		assert.Equal(t, byte(0xaa), one)
+		assert.Equal(t, byte(0xdd), two)
+	})
+
+	t.Run("reading two 2 bit and then one 8 bit", func(t *testing.T) {
+		bb := NewBitBufferFromBytes([]byte{0x6a, 0xa0})
+
+		one, _ := bb.ReadBits(2)
+		two, _ := bb.ReadBits(2)
+		three, _ := bb.ReadByte()
+
+		assert.Equal(t, byte(0x01), one)
+		assert.Equal(t, byte(0x02), two)
+		assert.Equal(t, byte(0xaa), three)
+	})
 }
