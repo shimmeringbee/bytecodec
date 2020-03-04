845d98435447e8aeb3de1dfa26221fdd25602d5b
Peter Wood <peter@alastria.net>
2020-03-04 19:27:51 +0000

    Add support to bitbuffer for reading 2's complement signed integers.

---
+++ bitbuffer/int.go
@@ -0,0 +1,52 @@
+package bitbuffer
+
+import (
+	"errors"
+)
+
+var ErrorNotByteMultiple = errors.New("can not read a non byte multiple int")
+
+func checkByteMultiple(length int) error {
+	if length%8 != 0 {
+		return ErrorNotByteMultiple
+	}
+
+	return nil
+}
+
+func (bb *BitBuffer) WriteInt(value int64, endian Endian, length int) error {
+	if err := checkByteMultiple(length); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (bb *BitBuffer) ReadInt(endian Endian, length int) (int64, error) {
+	if err := checkByteMultiple(length); err != nil {
+		return 0, err
+	}
+
+	var v int64
+	bytes := length / 8
+
+	if endian == BigEndian {
+		for i := 0; i < bytes; i++ {
+			if t, err := bb.ReadByte(); err != nil {
+				return 0, nil
+			} else {
+				v = v | int64(int8(t))<<byte((bytes-i-1)*8)
+			}
+		}
+	} else {
+		for i := 0; i < bytes; i++ {
+			if t, err := bb.ReadByte(); err != nil {
+				return 0, nil
+			} else {
+				v = v | int64(int8(t))<<byte(i*8)
+			}
+		}
+	}
+
+	return v, nil
+}
